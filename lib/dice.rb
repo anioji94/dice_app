9edafe6fa5af903df344db6f3a7953a92fb3b1f2
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-02-05 12:09:31 +0100

    more user stories tested

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,7 +1,14 @@
-class Dice 
+class Dice
 
-	def roll 
-		rand(1..6)
-	end 
+@@history = []
 
-end 
+	def roll(n = 1)
+   @roll = n.times.map {rand(1..6)}
+  end
+
+	def save_roll
+		@@history.push(@roll)
+	end
+
+
+end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,23 +1,29 @@
 # objects = Dice
-# message = Roll 
+# message = Roll
 
 require 'dice'
 
-describe Dice do 
+describe Dice do
 
 	it "should make a new dice" do
-		expect(Dice.new.is_a?(Dice)).to eq true 
+		expect(Dice.new.is_a?(Dice)).to eq true
 	end
-	
-	it "should roll" do 
+
+	it "should roll" do
 		expect(Dice.new).to respond_to(:roll)
-	end 
+	end
 
-	it "should give a number between 1 and 6" do 
-		expect(Dice.new.roll).to be <= 6 
-	end 
+	it "should give a number between 1 and 6" do
+		expect(Dice.new.roll[0]).to be <= 6
+	end
 
-	it "should roll any number of dice at the same time" do 
-		expect(Dice.new.roll(1..n)).to 
-	end 
-end 
+	it "should roll any number of dice at the same time" do
+		expect(Dice.new.roll(3)).to be_instance_of Array
+	end
+
+	it "should record all dice rolls" do
+		dice = subject
+	  dice.roll(4)
+		expect(dice.save_roll).to be_instance_of Array
+	end
+end
